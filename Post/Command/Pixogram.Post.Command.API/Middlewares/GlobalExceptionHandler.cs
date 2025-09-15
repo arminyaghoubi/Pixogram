bb1597b518e5a00efc107219bc46535f7ca5d7ab
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2025-09-15 21:36:33 +0330

    Global Exception implemented in Command API.

---
+++ Post/Command/Pixogram.Post.Command.API/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pixogram.Post.Command.API.Middlewares;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellation)
+    {
+        _logger.LogError("Exception: {Message}", exception.Message);
+
+        var problems = new ProblemDetails
+        {
+            Detail = exception.Message
+        };
+
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        await httpContext.Response.WriteAsJsonAsync(problems, cancellation);
+
+        return true;
+    }
+}
--- Post/Command/Pixogram.Post.Command.API/Program.cs
+++ Post/Command/Pixogram.Post.Command.API/Program.cs
@@ -1,16 +1,20 @@
 using Pixogram.Post.Command.Infrastructure;
 using Pixogram.Post.Command.API.Endpoints;
 using Pixogram.Post.Command.Application;
+using Pixogram.Post.Command.API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddInfrastructureServices(builder.Configuration)
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>()
+    .AddProblemDetails()
+    .AddInfrastructureServices(builder.Configuration)
     .AddApplicationServices();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseExceptionHandler();
 
 app.UseHttpsRedirection();
 
